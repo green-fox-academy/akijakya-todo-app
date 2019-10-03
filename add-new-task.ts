b408a009a01572d665d11f45747aac6e7b246e14
Andras Jaky <jakyandras@gmail.com>
2019-10-03 12:37:51 +0200

    fix(todo-app): fixed the line break problem after each line

--- add-new-task.ts
+++ add-new-task.ts
@@ -1,5 +1,10 @@
 const fs = require ('fs');
 
 export function addNewTask (todo:string) {
-    fs.appendFileSync('todos.txt', todo +'\n', 'utf8');
+    let todoLines:string [] = fs.readFileSync('todos.txt', 'utf8').split('\n');
+    if (todoLines.length <= 1 && todoLines[0] === '') {
+        fs.appendFileSync('todos.txt', todo, 'utf8');
+    } else {
+        fs.appendFileSync('todos.txt', '\n' + todo, 'utf8');
+    }
 }
