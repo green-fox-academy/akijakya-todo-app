9945fedea2fb53c5673f45fcdb047293e5fb7475
Andras Jaky <jakyandras@gmail.com>
2019-10-03 11:16:37 +0200

    feat(todo-app): print usage function

---
+++ main.ts
@@ -0,0 +1,26 @@
+'use strict';
+
+const args: string[] = process.argv; //for using command line arguments. Don't forget that it is a string array w/ atleast 2 elements already!!!
+
+
+
+function printUsage () {
+    if (args.length === 2) {
+        console.log('Command Line Todo application');
+        console.log('=============================\n');
+        console.log('Command line arguments:');
+        console.log('    -l    Lists all the tasks\n    -a    Adds a new task\n    -r    Removes a task\n    -c    Completes a task');
+    } else if (args[2] === '-l'){
+        // here comes the call for a function
+    } else if (args[2] === '-a'){
+        // here comes the call for a function
+    } else if (args[2] === '-r'){
+        // here comes the call for a function
+    } else if (args[2] === '-l'){
+        // here comes the call for a function
+    } else {
+        console.log('this is the error handling');
+  }
+}
+
+printUsage();
