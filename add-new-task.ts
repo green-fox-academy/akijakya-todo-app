57caff8020112ba6a5c6b843dacf8dff1ceed258
Andras Jaky <jakyandras@gmail.com>
2019-10-03 16:31:23 +0200

    added empty checkboxes when a new task is created

--- add-new-task.ts
+++ add-new-task.ts
@@ -3,8 +3,8 @@ const fs = require ('fs');
 export function addNewTask (todo:string) {
     let todoLines:string [] = fs.readFileSync('todos.txt', 'utf8').split('\n');
     if (todoLines.length <= 1 && todoLines[0] === '') {
-        fs.appendFileSync('todos.txt', todo, 'utf8');
+        fs.appendFileSync('todos.txt', '[ ] ' + todo, 'utf8');
     } else {
-        fs.appendFileSync('todos.txt', '\n' + todo, 'utf8');
+        fs.appendFileSync('todos.txt', '\n[ ] ' + todo, 'utf8');
     }
 }
