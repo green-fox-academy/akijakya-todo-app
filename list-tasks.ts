8d4d307932816b373446ec9c37216414a868d48a
Andras Jaky <jakyandras@gmail.com>
2019-10-03 11:44:52 +0200

    feat(todo-app): added message to -l argument when the todo list is empty

--- list-tasks.ts
+++ list-tasks.ts
@@ -2,7 +2,12 @@ const fs = require ('fs');
 
 export function listTasks () {
     let todoLines:string [] = fs.readFileSync('todos.txt', 'utf8').split('\r\n');
-    todoLines.forEach(function (e, i) {
-        console.log(i+1 + ' - ' + e);
-    });
+    //console.log(todoLines);
+    if (todoLines.length <= 1 && todoLines[0] === '') {
+        console.log('No todos for today! :)');
+    } else {
+        todoLines.forEach(function (e, i) {
+            console.log(i+1 + ' - ' + e);
+        });
+    }
 }
